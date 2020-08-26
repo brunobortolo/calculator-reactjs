1bf8aa801a74d68face5ef38a8ac0619103375a8
Bruno Bortolo <brunobortolinho@gmail.com>
2020-08-26 17:23:44 -0300

    Github Pages setup

---
+++ docs/precache-manifest.24a1082d1c009050e608c6bab184b1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6977dbeeb7f7dadf7f3dcc0a84af590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dbc60cfe9cd2a13838b",
+    "url": "/static/css/main.4bb21d07.chunk.css"
+  },
+  {
+    "revision": "951072c986f469110060",
+    "url": "/static/js/2.a011838f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a011838f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbc60cfe9cd2a13838b",
+    "url": "/static/js/main.3279c33f.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
---
+++ docs/static/js/main.3279c33f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[0],[,,,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){},function(e,n,t){"use strict";t.r(n);var c=t(0),r=t.n(c),o=t(2),a=t.n(o),u=(t(8),function(e){return r.a.createElement("p",null,e.content)});t(9);var l=function(){return r.a.createElement(u,{content:"0"})};a.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(l,null)),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.3279c33f.chunk.js.map
