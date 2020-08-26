9fddbbccb63778862e2acf82cc86982a8b9e7397
Bruno Bortolo <brunobortolinho@gmail.com>
2020-08-26 17:39:58 -0300

    Updates

---
+++ precache-manifest.4e61bd64cfc7546e95abeffe7a95d41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b89a235f2cd781c0be84a76d44af1b0",
+    "url": "/calculator-reactjs/index.html"
+  },
+  {
+    "revision": "0dbc60cfe9cd2a13838b",
+    "url": "/calculator-reactjs/static/css/main.4bb21d07.chunk.css"
+  },
+  {
+    "revision": "951072c986f469110060",
+    "url": "/calculator-reactjs/static/js/2.a011838f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-reactjs/static/js/2.a011838f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbc60cfe9cd2a13838b",
+    "url": "/calculator-reactjs/static/js/main.3279c33f.chunk.js"
+  },
+  {
+    "revision": "5458208324326c01c70a",
+    "url": "/calculator-reactjs/static/js/runtime-main.bcea1ba7.js"
+  }
+]);
